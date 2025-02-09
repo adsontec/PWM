faf4ec113f9e2880f1ad3a6a75a1b043454b78ad
Adson <adson.oliveira.inf@gmail.com>
2025-02-09 03:29:44 -0300

    Pronto

--- PWM.c
+++ PWM.c
@@ -1,52 +1,27 @@
 #include <stdio.h> // Biblioteca padrão da linguagem C
 #include "pico/stdlib.h" // Subconjunto central de bibliotecas do SDK Pico
+#include "hardware/irq.h" // Biblioteca para gerenciamento de interrupções
 #include "hardware/pwm.h" // Biblioteca para controlar o hardware de PWM
+#include "pico/time.h" // Necessário para usar get_absolute_time()
+#include "./includes/motor.h"
 
-#define PWM_MOTOR 22 // Pino do servo motor conectado a GPIO como PWM
-#define PWM_WRAP 20000 // Valor máximo do contador - WRAP
+#define PULSE_MAX 2400  // Pulso para 180 graus (2400us) - Duty Cycle - 0,12%
+#define PULSE_90 1470   // Pulso para 90 graus (1470us) - Duty Cycle - 0,0735%
+#define PULSE_MIN 500   // Pulso para 0 graus (500us) - Duty Cycle - 0,025%
 
-// Função para configurar o módulo PWM
-void setup_pwm() {
-    gpio_set_function(PWM_MOTOR, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(PWM_MOTOR);
-    pwm_set_wrap(slice_num, PWM_WRAP);
-    pwm_set_clkdiv(slice_num, 125.0f);
-    pwm_set_enabled(slice_num, true);
-}
 
-// Função do ciclo ativo
-void set_pwm_duty_cycle(uint slice_num, uint16_t duty_cycle) {
-    pwm_set_gpio_level(PWM_MOTOR, duty_cycle);
-}
-
-// Função para move o servo motor
-void move_servo(uint16_t duty_cycle) {
-    uint slice_num = pwm_gpio_to_slice_num(PWM_MOTOR);
-    set_pwm_duty_cycle(slice_num, duty_cycle);
-    sleep_ms(1000);
-}
-
-// Rotina principal
-int main(){
+int main() {
     stdio_init_all(); // Inicializa o sistema padrão de I/O
-    setup_pwm(); // Configura o PWM
 
-    move_servo(2400); // 180°
-    move_servo(1470); // 90°
-    move_servo(500);  // 0°
+    setup_pwm(); // Configura o PWM ANTES de mover o servo
 
-    // Loop principal
+    move_servo(PULSE_MAX); // 180°
+    move_servo(PULSE_90);  // 90°
+    move_servo(PULSE_MIN); // 0°
+
+    pwm_setup_irq(); // Agora inicializa a interrupção
+
     while (1) {
-        /*
-        for (uint16_t duty_cycle = 500; duty_cycle <= 2400; duty_cycle += 5) {
-            set_pwm_duty_cycle(pwm_gpio_to_slice_num(PWM_MOTOR), duty_cycle);
-            sleep_ms(10);
-        }
-        for (uint16_t duty_cycle = 2400; duty_cycle >= 500; duty_cycle -= 5) {
-            set_pwm_duty_cycle(pwm_gpio_to_slice_num(PWM_MOTOR), duty_cycle);
-            sleep_ms(10);
-        }
-        */
+        
     }
-    return 0;
 }
